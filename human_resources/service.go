c5d8e9b64d6451bc04b2482ae8788d9f0e360fae
Martin Lehoux <martin.lehoux@gojob.com>
2022-04-13 00:05:46 +0200

    refactor: Move package user to human_resources

--- human_resources/service.go
+++ human_resources/service.go
@@ -1,4 +1,4 @@
-package user
+package human_resources
 
 import (
 	"errors"
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"go-microservices/authentication"
 	"go-microservices/common"
-	"go-microservices/user"
+	"go-microservices/human_resources"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,8 +13,8 @@ func main() {
 	app := fiber.New()
 	authenticationService := authentication.Bootstrap(common.LoadPrivateKey("id_rsa"))
 	authentication.BootstrapHttpController(app.Group("/auth"), authenticationService)
-	userService := user.Bootstrap()
-	user.BootstrapHttpController(app.Group("/users"), userService)
+	userService := human_resources.Bootstrap()
+	human_resources.BootstrapHttpController(app.Group("/users"), userService)
 
 	app.Get("/ping", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("pong")
