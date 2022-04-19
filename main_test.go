3c6b7114f71e4d7548a1ec7d2eaab994962aee39
Martin Lehoux <martin.lehoux@gojob.com>
2022-04-19 22:19:36 +0200

    test: Add first e2e tests

---
+++ main_test.go
@@ -0,0 +1,24 @@
+//go:build e2e
+
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPing(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run("it should respond with 'pong'", func(t *testing.T) {
+		res, err := http.DefaultClient.Get("http://localhost:3000/ping")
+
+		assert.NoError(err)
+		assert.Equal(200, res.StatusCode)
+		body, _ := ioutil.ReadAll(res.Body)
+		assert.Equal("pong", string(body))
+	})
+}
