797d7c3bab321644e352ff4d1624f18db17b7437
Martin Lehoux <martin.lehoux@gojob.com>
2022-04-19 19:19:38 +0200

    feat(common): ParseToken from a request

--- common/token_test.go
+++ common/token_test.go
@@ -65,3 +65,51 @@ func TestParseToken(t *testing.T) {
 		assert.Equal(token, Token{}, "the token should be empty")
 	})
 }
+
+func TestExtractToken(t *testing.T) {
+	assert := assert.New(t)
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 1024)
+	publicKey := privateKey.PublicKey
+
+	t.Run("it should error if there is no Authorization header", func(t *testing.T) {
+		req := PrepareRequest("POST", "/any", nil)
+
+		token, err := ExtractToken(req.Header.Get("Authorization"), publicKey)
+		assert.ErrorContains(err, "request has no authorization header")
+		assert.Equal(Token{}, token)
+	})
+
+	t.Run("it should error if the header is missformed", func(t *testing.T) {
+		req := PrepareRequest("POST", "/any", nil)
+		req.Header.Add("Authorization", "Whatever")
+
+		token, err := ExtractToken(req.Header.Get("Authorization"), publicKey)
+		assert.ErrorContains(err, "request has invalid authorization header")
+		assert.Equal(Token{}, token)
+	})
+
+	t.Run("it should error if the token is invalid", func(t *testing.T) {
+		req := PrepareRequest("POST", "/any", nil)
+		req.Header.Add("Authorization", "Bearer ABC")
+
+		token, err := ExtractToken(req.Header.Get("Authorization"), publicKey)
+		assert.ErrorContains(err, "invalid character")
+		assert.Equal(Token{}, token)
+	})
+
+	t.Run("it should return the token if it is valid", func(t *testing.T) {
+		token := Token{CreatedAt: time.Now(), Identifier: "test"}
+		blob, _ := SignToken(token, *privateKey)
+		req := PrepareRequest("POST", "/any", nil)
+		req.Header.Add("Authorization", "Bearer "+string(blob))
+
+		parsedToken, err := ExtractToken(req.Header.Get("Authorization"), publicKey)
+		assert.NoError(err, "the parsing should be successful")
+		assert.True(token.CreatedAt.Equal(parsedToken.CreatedAt), "the creation date should match")
+		assert.Equal(token.Identifier, parsedToken.Identifier, "the identifier should match")
+	})
+
+	t.Run("it should error if the token is expired", func(t *testing.T) {
+		t.Skip()
+	})
+}
